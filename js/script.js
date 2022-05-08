e3c1aea2bab1e3df2417a3180f9c2935b649120c
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-05-08 16:13:38 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -1,21 +1,35 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+/* Created by: Emmanuel.fofeyin
+ * Created on: Apr 2022
+ * This file contains the JS functions for index.html 
+*/
+
+'use strict'
 
 /**
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/github-page-pwa/sw.js", {
-    scope: "/github-page-pwa/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-03-HTML/sw.js", {
+    scope: "/ICS2O-Unit5-03-HTML/",
   })
 }
 
 /**
- * This function displays an alert.
+ * This function determines if a person is eligible for movie rates.
  */
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  //input
+  const seventeenAbove = document.getElementById("seventeenAbove").checked
+  const thirteenToSixteen = document.getElementById("thirteenToSixteen").checked
+  const fiveToTwelve = document.getElementById("fiveToTwelve").checked
+  //process
+  if (seventeenAbove == true) {
+    document.getElementById("answers").innerHTML = "You are eligible to watch R rated movies alone."
+  } else if (thirteenToSixteen == true) {
+      document.getElementById("answers").innerHTML = "You are eligible to watch PG 13 rated movies alone."
+  } else if (fiveToTwelve == true) {
+      document.getElementById("answers").innerHTML = "You are eligible to watch G and PG rated movies alone."
+  } else {
+      document.getElementById("answers").innerHTML = "You are too young to watch any rated movies alone."
+  }
 }
